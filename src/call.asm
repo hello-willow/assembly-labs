b5f621d0fed101a4de07bafc68113430d829242e
willow <willow20220101@gmail.com>
2022-02-20 13:54:43 -0500

    polish call & add hw files

--- src/call.asm
+++ src/call.asm
@@ -1,12 +1,12 @@
 global _start
 
 section .data
-    msg: db "Call to exit.", 0xa
+    msg: db "Call to exit, with return value of 101.", 0xa
     len: equ $ - msg
 
 section .text
 exit_101:           ; function to load ebx & eax with exit vals
-    mov ebx, 101    ; exit code
+    mov ebx, 101    ; load 101 as return value
     mov eax, 1      ; sys_exit 
     ret
 

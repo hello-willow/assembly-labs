83b336a7ab40b96ad0645d89096f8c96757cda36
willow <willow20220101@gmail.com>
2022-03-12 17:23:34 -0500

    lab 4, branch.asm

--- src/branch.asm
+++ src/branch.asm
@@ -5,14 +5,22 @@ section .data
     hello_len: equ $ - hello_msg
     bye_msg: db "Goodbye, Clarice.", 0xa
     bye_len: equ $ - bye_msg
-    test: db 0
+    test: db 1
 
 section .text
 bye:
     ; save stack base ptr
+    push ebp
     ; move new stack ptr to be the called
+    mov ebp, esp
     ; write goodbye msg
+    mov edx, bye_len
+    mov ecx, bye_msg
+    mov al, 4               ; sys_write
+    mov ebx, 1              ; stdout
+    int 0x80
     ; restore base btr and return
+    pop ebp
 
 exit:
     mov al, 1               ; sys_exit
@@ -23,7 +31,6 @@ _start:
     mov al, [test]
     cmp al, 1
     jz bye
-
 
     mov edx, hello_len
     mov ecx, hello_msg

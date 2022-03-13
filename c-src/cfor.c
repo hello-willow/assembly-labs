cb7c4354d7afde192ecc8eedda851bd4d21ffac5
willow <willow20220101@gmail.com>
2022-03-13 16:36:03 -0400

    c code clean-up

--- c-src/cfor.c
+++ c-src/cfor.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 int main() {
-    int loop_count = 5;
-    int size_hello = 12;
-    int size_goodbye = 15;
-    char* hello = "Hello, Clarice.";
-    char* goodbye = "Goodbye, Clarice.";
+    int i;
+    int loop_count = 3;
+    char* hello = "Hello, Clarice.\n";
+    char* goodbye = "Goodbye, Clarice.\n";
+    size_t size_hello = strlen(hello);
+    size_t size_goodbye = strlen(goodbye);
 
-    for(int i = 0; i != loop_count; i++) {
+    for(i = 0; i != loop_count; i++) {
         write(1, hello, size_hello);
     }
 
--- c-src/cgoodbye.c
+++ c-src/cgoodbye.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 int main() {
     int test = 1;
-    int size_hello = 12;
-    int size_goodbye = 15;
-    char* hello = "Hello, Clarice.";
-    char* goodbye = "Goodbye, Clarice.";
+    char* hello = "Hello, Clarice.\n";
+    char* goodbye = "Goodbye, Clarice.\n";
+    size_t size_hello = strlen(hello);
+    size_t size_goodbye = strlen(goodbye);
 
     if(test) {
         write(1, hello, size_hello);

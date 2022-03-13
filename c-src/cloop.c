5eaf0ab69fe2484b320bdc8fbfc5cbd4bdf5040b
willow <willow20220101@gmail.com>
2022-03-13 15:24:35 -0400

    clean up cloop.c

--- c-src/cloop.c
+++ c-src/cloop.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 int main() {
-    int loop_count = 5;
-    int size_count = 12;
-    int size_hello = 15;
-    int size_goodbye = 17;
-    char* hello = "Hello, Clarice.";
-    char* goodbye = "Goodbye, Clarice.";
+    int i;
+    char* hello = "Hello, Clarice.\n";
+    char* goodbye = "Goodbye, Clarice.\n";
+    size_t size_hello = strlen(hello);
+    size_t size_goodbye = strlen(goodbye);
 
-    while(loop_count) {
+    i = 3;
+    while(i) {
         write(1, hello, size_hello);
-        loop_count--;
+        i--;
     }
 
     write(1, goodbye, size_goodbye);
